f764560450a2baf748d07e6b8d40794f65f9ba55
XploreTV2go <xploretv2go@gmail.com>
2021-01-18 13:35:45 +0100

    Update content elements

--- _functions/acf_content_elements.php
+++ _functions/acf_content_elements.php
@@ -28,7 +28,7 @@ function content_element_section_b($all_data) {
             <div class="h1 h-bold"><?= $all_data['headline'] ?></div>
             <h3><?= $all_data['copytext'] ?></h3>
         </div>
-        <div id="js-a1xploretv-k-slider" class="a1xploretv-k-slider">
+        <div id="js-a1xploretv-k-slider" class="a1xploretv-k-slider js-a1xploretv-k-slider">
           <?php foreach ($all_data['cards'] as $card) { ?>
             <a href="<?= $card['card-href'] ?>" class="focusable">
                 <img src="<?= $card['image']['url'] ?>" alt="<?= $card['image']['alt'] ?>">
@@ -84,18 +84,37 @@ function content_element_section_c($all_data) {
 // Section D - Full width video
 function content_element_section_d($all_data) {
     ?>
-    <section id="a1xploretv-video-section" class="a1xploretv-d">
-        <video id="js-a1xploretv-d-video" playsinline  poster="<?= $all_data['video-poster']['url'] ?>" id="bgvid">
-          <source src="<?= $all_data['video-source'] ?>" type="video/mp4">
-        </video>
+    <section class="a1xploretv-d a1xploretv-video-section">
+        <?php
+          if ($all_data['video-provider'] == 'local-video') {
+        ?>
+          <video class="js-a1xploretv-d-video" playsinline  poster="<?= $all_data['video-poster']['url'] ?>" id="bgvid">
+            <source src="<?= $all_data['video-source'] ?>" type="video/mp4">
+          </video>
+        <?php
+          } else if ($all_data['video-provider'] == 'stream') {
+        ?>
+          <video class="js-a1xploretv-d-video" playsinline  poster="<?= $all_data['video-poster']['url'] ?>" id="bgvid">
+            <source src="<?= $all_data['video-stream'] ?>" type="video/mp4">
+          </video>
+        <?php
+          } else if ($all_data['video-provider'] == 'youtube' || $all_data['video-provider'] == 'vimeo') {
+        ?>
+            <div class="a1xploretv-video-frame">
+                <?php $base_url = ($all_data['video-provider'] == 'youtube') ? 'https://www.youtube.com/embed/' : 'https://player.vimeo.com/video/'; ?>
+                <iframe width="100%" frameborder="0" allowfullscreen allow="autoplay" data-type="<?=$all_data['video-provider']?>"  data-src="<?= $base_url . $all_data['video-id'] ?>" src="<?= $base_url . $all_data['video-id'] ?>"></iframe>
+            </div>
+        <?php
+          }
+        ?>
 
-        <div id="js-a1xploretv-d-content" class="position-relative d-flex flex-column justify-content-center align-items-center text-center h-100 ">
+        <div class="js-a1xploretv-d-content position-relative d-flex flex-column justify-content-center align-items-center text-center h-100 ">
             <div class="h1 text-white h-bold"><?= $all_data['headline'] ?></div>
             <div class="mt-1">
                 <h3 class="text-white mb-5">
                     <?= $all_data['copytext'] ?>
                 </h3>
-                <a href="javascript:void(0)" id="js-a1xploretv-d-start" class="a1xploretv-d-play mx-auto focusable"></a>
+                <a href="javascript:void(0)"  class="a1xploretv-d-play mx-auto focusable js-a1xploretv-d-start"></a>
             </div>
         </div>
     </section>
@@ -109,14 +128,14 @@ function content_element_section_e($all_data) {
         <div class="d-flex flex-column justify-content-center align-items-center mb-4">
             <div class="h1 h-bold"><?= $all_data['headline'] ?></div>
         </div>
-        <div id="js-a1xploretv-l-slider" class="a1xploretv-l-slider">
+        <div id="js-a1xploretv-l-slider" class="a1xploretv-l-slider js-a1xploretv-l-slider">
             <?php
               $num = 0;
               foreach ($all_data['cards'] as $card) {
             ?>
 
             <div class="product" id="<?= $num ?>" >
-                <img <?php if ($num === 0) { ?> id="set-first"<?php } ?> class="focusable" src="<?= $card['image']['url'] ?>" alt="<?= $card['image']['alt'] ?>" data-sn-up="#js-a1xploretv-d-start">
+                <img <?php if ($num === 0) { ?> id="set-first"<?php } ?> class="focusable" src="<?= $card['image']['url'] ?>" alt="<?= $card['image']['alt'] ?>">
             </div>
 
             <?php
@@ -251,38 +270,30 @@ function content_element_section_h($all_data) {
         <div class="a1xploretv-e-inner">
           <div class="a1xploretv-e-form">
               <form name="a1xploretv-e-form" method="POST" action="#" autocomplete="off">
+                  <?php
+                    foreach ($all_data['question-blocks'] as $question_block) {
+                  ?>
+                      <h4><?= $question_block['question'] ?></h4>
+                      <input name="questions[]" type="hidden" value="<?= $question_block['question'] ?>">
+                  <?php
+                    }
+                  ?>
                   <div class="a1xploretv-e-checkbox-group">
                       <div class="a1xploretv-e-checkboxes">
+                        <?php
+                          $num = 0;
+                          foreach ($question_block['answers'] as $answer) {
+                        ?>
                           <label class="a1xploretv-e-checkbox focusable" tabindex="-1">
-                              <input type="checkbox" value="Gleam" name="brands[]">
+                              <input type="checkbox" value="<?= $answer['answer'] ?>" name="answers-<?= $num ?>[]">
                               <span class="a1xploretv-e-checkbox-ui-bg" ></span>
                               <span class="a1xploretv-e-checkbox-ui "></span>
-                              <span class="a1xploretv-e-checkbox-text ">Gleam</span>
+                              <span class="a1xploretv-e-checkbox-text "><?= $answer['answer'] ?></span>
                           </label>
-                          <label class="a1xploretv-e-checkbox focusable" tabindex="-1">
-                              <input type="checkbox" value="c'Balm" name="brands[]">
-                              <span class="a1xploretv-e-checkbox-ui-bg"></span>
-                              <span class="a1xploretv-e-checkbox-ui "></span>
-                              <span class="a1xploretv-e-checkbox-text">c'Balm</span>
-                          </label>
-                          <label class="a1xploretv-e-checkbox focusable" tabindex="-1">
-                              <input type="checkbox" value="Bon Organics" name="brands[]">
-                              <span class="a1xploretv-e-checkbox-ui-bg"></span>
-                              <span class="a1xploretv-e-checkbox-ui "></span>
-                              <span class="a1xploretv-e-checkbox-text">Bon Organics</span>
-                          </label>
-                          <label class="a1xploretv-e-checkbox focusable" tabindex="-1">
-                              <input type="checkbox" value="Hera" name="brands[]">
-                              <span class="a1xploretv-e-checkbox-ui-bg"></span>
-                              <span class="a1xploretv-e-checkbox-ui "></span>
-                              <span class="a1xploretv-e-checkbox-text">Hera</span>
-                          </label>
-                          <label class="a1xploretv-e-checkbox focusable" tabindex="-1">
-                              <input type="checkbox" value="None of the Above" name="brands[]">
-                              <span class="a1xploretv-e-checkbox-ui-bg"></span>
-                              <span class="a1xploretv-e-checkbox-ui "></span>
-                              <span class="a1xploretv-e-checkbox-text">None of the Above</span>
-                          </label>
+                        <?php
+                            $num++;
+                          }
+                        ?>
                       </div>
                   </div>
                   <div class="a1xploretv-e-form-btns">
